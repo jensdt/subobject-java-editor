e68bc0cfa1934d9df5a8143eaf8340fac2732b65
marko <marko@dhcp-10-9-32-126.kotnet.kuleuven.be>
2010-02-11 18:43:25 +0100

    update

--- src/subobjectjava/eclipse/Bootstrapper.java
+++ src/subobjectjava/eclipse/Bootstrapper.java
@@ -1,12 +1,15 @@
 package subobjectjava.eclipse;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 
 import subobjectjava.input.SubobjectJavaModelFactory;
 import subobjectjava.model.language.SubobjectJava;
 import chameleon.core.language.Language;
 import chameleon.editor.connector.EclipseBootstrapper;
 import chameleon.editor.connector.EclipseEditorExtension;
+import chameleon.exception.ChameleonProgrammerException;
 import chameleon.input.ModelFactory;
 import chameleon.input.ParseException;
 import chameleon.output.Syntax;
@@ -42,6 +45,11 @@ public class Bootstrapper implements EclipseBootstrapper {
 		SubobjectJava result = new SubobjectJava();
 		ModelFactory factory = new SubobjectJavaModelFactory(result);
 		factory.setLanguage(result, ModelFactory.class);
+		try {
+		  factory.initializeBase(new ArrayList<File>());
+		} catch(ChameleonProgrammerException exc) {
+			// Object and String may not be present yet.
+		}
 		result.setConnector(EclipseEditorExtension.class, new SubobjectJavaEditorExtension());
 		return result;
 	}
