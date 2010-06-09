65bb0016df421384b1c04ff58f451a50ec6dbdfa
Marko van Dooren <Marko.vanDooren@cs.kuleuven.be>
2010-06-09 15:13:53 +0200

    fixes and added icons

--- src/subobjectjava/eclipse/SubobjectJavaEditorExtension.java
+++ src/subobjectjava/eclipse/SubobjectJavaEditorExtension.java
@@ -1,22 +1,52 @@
 package subobjectjava.eclipse;
 
+import java.io.IOException;
+
+import jnome.editor.JavaEditorExtension;
+
+import org.eclipse.swt.graphics.Image;
+
 import subobjectjava.model.component.ComponentRelation;
 import chameleon.core.element.Element;
-import jnome.editor.JavaEditorExtension;
 
 public class SubobjectJavaEditorExtension extends JavaEditorExtension {
 
 	@Override
 	public String getLabel(Element element) {
 		try {
-//		if(element instanceof ComponentRelation) {
-//			return ((ComponentRelation)element).signature().name();
-//		} else {
 			return super.getLabel(element);
-//		}
 		} catch(NullPointerException exc) {
 			return "";
 		}
+	}
+	
+	
+
+	@Override
+	protected void initializeRegistry() {
+		super.initializeRegistry();
+		try {
+			register("component");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+
+  @Override
+	public Image getIcon(Element element)  {
+  	Image result = null;
+  	if(element instanceof ComponentRelation) {
+		  result = imageRegistry().get("component");
+  	}
+  	return result;
+	}
+
+
+
+	@Override
+	public String pluginID() {
+		return Bootstrapper.PLUGIN_ID;
 	}
 
 }
